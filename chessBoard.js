fd3c2d42f6076a7b9a02abcaf518aadc1626b309
AlexVedernikoff <templar_sc@mail.ru>
2023-11-14 01:17:19 +0300

    рефактор задачи про коня

--- chessBoard.js
+++ chessBoard.js
@@ -1,112 +1,85 @@
-const arrX = ["a", "b", "c", "d", "e", "f", "g", "h"];
-const arrY = [8, 7, 6, 5, 4, 3, 2, 1];
-const main = document.querySelector('main');
+"use strict";
+const labelsArray = ["a", "b", "c", "d", "e", "f", "g", "h"];
+const body = document.querySelector("body");
 const movingsArray = [];
-
-//создаём шахматную доску
-arrY.forEach((elRow, j) => {
-    const row = document.createElement('div');
-    const rowIndex = document.createElement('div');
-    rowIndex.innerText = elRow;
-    main.appendChild(row);
+function createRow(r) {
+    const row = document.createElement("div"); // создаём горизонтальный ряд клеток
+    row.classList.add("row");
+    const rowIndex = document.createElement("div"); // цифровая подпись 1-8 слева от ряда
+    rowIndex.classList.add("rowIndex");
+    rowIndex.innerText = String(r + 1);
     row.appendChild(rowIndex);
-    rowIndex.classList.add('rowIndex');
-    row.classList.add("row")
-    arrX.forEach((el, i) => {
-        const cell = document.createElement('div');
+    labelsArray.forEach((_, c) => {
+        // поочерёдно создаём 8 ячеек шахматной доски и добавляем их в ряд row.
+        const cell = document.createElement("div");
+        cell.classList.add("cell");
+        (r + c) % 2 === 0
+            ? cell.classList.add("dark")
+            : cell.classList.add("light");
+        cell.id = `${r}${c}`;
         row.appendChild(cell);
-        cell.classList.add("cell")
-        cell.id = el + elRow;
-    
-        if (j%2 == 0) {
-            if (i%2 == 0) {
-                cell.classList.add('light');
-            } else {
-                cell.classList.add('dark');
-            }
-        } else {
-            if (i%2 != 0) {
-                cell.classList.add('light');
-            } else {
-                cell.classList.add('dark');
-            }
-        }
-    })
-})
-
-//Добавялем подписи a - h по горизонтали
-const columnLetters = document.createElement('div');
-main.appendChild(columnLetters);
-columnLetters.classList.add('row')
-arrX.forEach(el => {
-    const letter = document.createElement('div');
-    letter.innerText = el;
-    columnLetters.appendChild(letter);
-    letter.classList.add('letter')
-})
-
-//создаём массив возможных передвижений коня
-const createMovings = (x, y) => {
-    //удаляем с доски предыдущие передвижения
-    movingsArray.forEach((el) => {        
-        const moving = document.getElementById(el);
-        console.log(moving)
-        if(moving) {
+    });
+    body === null || body === void 0 ? void 0 : body.appendChild(row);
+}
+function createLetterLabels() {
+    //ряд labelsRow содержит подписи a - h по горизонтали
+    const labelsRow = document.createElement("div");
+    labelsRow.classList.add("row");
+    labelsArray.forEach((letter) => {
+        const labelCell = document.createElement("div");
+        labelCell.classList.add("letter");
+        labelCell.innerText = letter;
+        labelsRow.appendChild(labelCell);
+    });
+    body === null || body === void 0 ? void 0 : body.appendChild(labelsRow);
+}
+function createChessboard() {
+    labelsArray.forEach((_, r) => {
+        createRow(7 - r);
+    });
+    createLetterLabels();
+}
+function createMovings(x, y) {
+    // создаём массив возможных передвижений коня
+    // удаляем с доски предыдущие передвижения
+    movingsArray.forEach((id) => {
+        const moving = document.getElementById(id);
+        if (moving)
             moving.classList.remove("moving");
-        }
-    })
+    });
     //очищаем массив передвижений с прошлого раза
-    if (movingsArray.length) {
-        movingsArray.length =0;
-    } ;
-
-    const X = arrX.findIndex(el => el == x);
-    const Y = arrY.findIndex(el => el == y);
-    const arr = [-2, -1, 1, 2];
-    
+    if (movingsArray.length)
+        movingsArray.length = 0;
+    const distanses = [1, 2].reduce((acc, el) => acc.concat(el, el * -1), []);
     //вычисляем новый массив передвижений
-    arr.forEach((elX) =>{
-        arr.forEach((elY)=>{
-            if ( (Math.abs(elX) + Math.abs(elY) == 3)) {
-                console.log(`elX = ${elX}, elY = ${elY}`)
-                console.log(`Y + elY = ${ Y + elY}`)
-                console.log(`X + elX = ${ X + elX}`)
-                if (arrX[X+elX] && arrY[Y+elY]) {
-                    movingsArray.push(String(arrX[X+elX]) + String(arrY[Y+elY]))
-                }
-            } 
-        })
-    })
-    console.log(`movingsArray = `);
-    console.log(movingsArray);
-
+    distanses.forEach((rowDist) => {
+        distanses.forEach((cellDist) => {
+            if (Math.abs(rowDist) + Math.abs(cellDist) === 3 &&
+                labelsArray[x + rowDist] &&
+                labelsArray[y + cellDist]) {
+                movingsArray.push(`${x + rowDist}${y + cellDist}`);
+            }
+        });
+    });
     //раскрашиваем клетки возможных передвижений коня
-    movingsArray.forEach((el) => {
-        const moving = document.getElementById(el);
-        console.log(moving)
-        if(moving) {
+    movingsArray.forEach((id) => {
+        const moving = document.getElementById(id);
+        if (moving) {
             moving.classList.add("moving");
         }
-    })
-
-};
-
-main.addEventListener("click", (event) => {
-    if (event.target.id) {
-        const checked = document.getElementById(event.target.id);
+    });
+}
+createChessboard();
+body === null || body === void 0 ? void 0 : body.addEventListener("click", (event) => {
+    const { id } = event.target;
+    if (id) {
+        const checked = document.getElementById(id);
         const previousCheck = document.querySelector(".checked");
         if (previousCheck) {
-            previousCheck.classList.remove('checked');
+            previousCheck.classList.remove("checked");
         }
-        const img = document.createElement('img');
-        checked.classList.add('checked');
-
-        console.log(`Координаты коня: ${event.target.id}`)
-
-        let [x, y] =  event.target.id.split("");
-        console.log(`Координата коня x: ${x}`);
-        console.log(`Координата коня y: ${y}`);
-
-        createMovings(x, y);
+        checked === null || checked === void 0 ? void 0 : checked.classList.add("checked");
+        let [x, y] = id.split("");
+        createMovings(Number(x), Number(y));
     }
-})
+});
